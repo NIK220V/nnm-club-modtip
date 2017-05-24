554daaddb2ff11c8c3a05fabbe0066061b0e8a73
Nicolas Angelov <nikovolt220@gmail.com>
2017-05-25 01:37:11 +0300

    Create nnm-modmenu.meta.js

---
+++ nnm-modmenu.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         NNM ModMenu Tip
+// @namespace    none
+// @version      0.01
+// @description  Скрипт, который будет показывать удобную менюшку при наведении на название темы в форумах, которые вы модерируете.
+// @author       NIK220V
+// @match        http://nnmclub.to/forum/viewforum.php?f=*
+// @match        https://nnmclub.to/forum/viewforum.php?f=*
+// @homepage     https://github.com/NIK220V/nnm-club-modtip
+// @updateURL    https://github.com/NIK220V/nnm-club-modtip/raw/master/nnm-modmenu.meta.js
+// @downloadURL  https://github.com/NIK220V/nnm-club-modtip/raw/master/nnm-modmenu.user.js
+// @run-at       document-start
+// @grant        none
+// ==/UserScript==
