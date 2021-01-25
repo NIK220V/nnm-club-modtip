5337f6c9565cc33c3c23ea1e545c64dc3089e2a0
Nicolas Angelov <nikovolt220@gmail.com>
2021-01-25 15:31:31 +0200

    Update nnm-modmenu.meta.js

--- nnm-modmenu.meta.js
+++ nnm-modmenu.meta.js
@@ -1,21 +1,19 @@
 // ==UserScript==
 // @name         NNM ModMenu Tip
 // @namespace    none
-// @version      0.06
+// @version      0.07
 // @description  Пак полезных функций.
 // @author       NIK220V
 // @match        *://*.nnmclub.to/forum/viewforum.php?f=*
 // @match        *://*.nnmclub.tv/forum/viewforum.php?f=*
 // @match        *://*.nnm-club.lib/forum/viewforum.php?f=*
-// @match        *://*.nnm-club.me/forum/viewforum.php?f=*
-// @match        *://*.nnm-club.name/forum/viewforum.php?f=*
+// @match        *://*.nnm-club.ro/forum/viewforum.php?f=*
 // @match        *://*.nnm-club.i2p.onion/forum/viewforum.php?f=*
 // @match        *://*.nnmclub5toro7u65.onion/forum/viewforum.php?f=*
 // @match        *://*.nnmclub.to/forum/tracker.php*
 // @match        *://*.nnmclub.tv/forum/tracker.php*
 // @match        *://*.nnm-club.lib/forum/tracker.php*
-// @match        *://*.nnm-club.name/forum/tracker.php*
-// @match        *://*.nnm-club.me/forum/tracker.php*
+// @match        *://*.nnm-club.ro/forum/tracker.php*
 // @match        *://*.nnm-club.i2p.onion/forum/tracker.php*
 // @match        *://*.nnmclub5toro7u65.onion/forum/tracker.php*
 // @homepage     https://github.com/NIK220V/nnm-club-modtip
